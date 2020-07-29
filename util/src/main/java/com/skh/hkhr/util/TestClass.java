d916e331bf0ddcd9f7d0d54c695f64a15542840d
samirankumar11 <samirankumar11@gmail.com>
2020-07-29 07:56:43 +0600

    Create lib

---
+++ util/src/main/java/com/skh/hkhr/util/TestClass.java
@@ -0,0 +1,14 @@
+package com.skh.hkhr.util;
+
+class TestClass {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
